4171ff0e7f7e561faab974e81899bd0278ce4716
@israman30 <you@example.com>
2023-12-10 20:18:33 -0500

    Rgit add . changed App entry Point as MainEntry

---
+++ iOSTestDevOps/iOSTestDevOps/Main.swift
@@ -0,0 +1,15 @@
+//
+//  Main.swift
+//  iOSTestDevOps
+//
+//  Created by Israel Manzo on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MainEntry {
+    static func main() {
+        iOSTestDevOpsApp.main()
+    }
+}
--- iOSTestDevOps/iOSTestDevOps/iOSTestDevOpsApp.swift
+++ iOSTestDevOps/iOSTestDevOps/iOSTestDevOpsApp.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-@main
 struct iOSTestDevOpsApp: App {
     var body: some Scene {
         WindowGroup {
