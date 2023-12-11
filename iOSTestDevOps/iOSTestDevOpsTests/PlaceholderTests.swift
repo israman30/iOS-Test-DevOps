8e0ced527c8bddcfe7ec5538e90e8cb8ab8578d7
@israman30 <you@example.com>
2023-12-10 20:41:20 -0500

    t - reaplce initial Test file

---
+++ iOSTestDevOps/iOSTestDevOpsTests/PlaceholderTests.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceholderTests.swift
+//  iOSTestDevOpsTests
+//
+//  Created by Israel Manzo on 12/10/23.
+//
+
+import XCTest
+@testable import iOSTestDevOps
+
+@MainActor
+final class PlaceholderTests: XCTestCase {
+
+    func test_zero() throws {
+
+    }
+
+}
