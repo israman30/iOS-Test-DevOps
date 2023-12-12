c6d417383c5cdbbd1913c4bc74d8d9b89a1071f5
@israman30 <you@example.com>
2023-12-11 19:53:31 -0500

    updated script file and README

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# xcodebuild test -project iOSTestDevOps.xcodeproj -scheme iOSTestDevOps -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 8"
+xcodebuild test -project iOSTestDevOps.xcodeproj -scheme iOSTestDevOps -sdk iphonesimulator -destination "platform=iOS Simulator,OS=latest,name=iPhone 15"
 
-xcodebuild \
-    -workspace iOSTestDevOps.xcworkspace \
-    -scheme iOSTestDevOps \
-    -sdk iphonesimulator \
-    -configuration Release \
-    clean build \
-    OBJROOT=$(PWD)/build \
-    SYMROOT=$(PWD)/build
+#xcodebuild \
+    # -workspace iOSTestDevOps.xcworkspace \
+    # -scheme iOSTestDevOps \
+    # -sdk iphonesimulator15 \
+    # -configuration Release \
+    # clean build \
+    # OBJROOT=$(PWD)/build \
+    # SYMROOT=$(PWD)/build
