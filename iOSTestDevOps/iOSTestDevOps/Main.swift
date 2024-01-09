363b6fd8486601ff61f7765054a166f302005f70
@israman30 <you@example.com>
2024-01-08 20:09:30 -0500

    r - extract main entry app is nil to method isProduction that returns a boolean

--- iOSTestDevOps/iOSTestDevOps/Main.swift
+++ iOSTestDevOps/iOSTestDevOps/Main.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 @main
 struct MainEntry {
+    
     static func main() {
         /// Use if and get out of the conditional
         ///  cna use if statement or guard
-        guard NSClassFromString("XCTestCase") == nil else {
+        guard isProduction() else {
             TestApp.main()
             return
         }
         iOSTestDevOpsApp.main()
+    }
+    
+    private static func isProduction() -> Bool {
+        return NSClassFromString("XCTestCase") == nil
     }
 }
